909f5ce5c06e90940ef3d4d5b60059f5798db10e
zd <946087009@qq.com>
2018-07-18 22:05:38 +0800

    tmp

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,35 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Base from '../Component/index';
 import commonCss from '../Jss/common';
 import cssData from '../MockData/css';
 import jsData from '../MockData/javascript';
-import testData from '../MockData/test'
+import testData from '../MockData/test';
+const selectStyle = {
+    display: "inline-block",
+    width: "120px",
+    height: "25px",
+    borderRadius: "5px",
+    marginRight: "10px"
+}
 export default class SelfLayout extends Component {
-    constructor (props){
+    constructor(props) {
         super(props);
         this.state = {
-            content: [],  /**保存原始数据 */                                                      
-            activeRow: 0, /** 当前正在输入的行数*/                                                     
-            activeWord: 0, /** 当前正在输入的单词下标*/                                                     
-            inputVelocity:0, /**速度*/
+            selectValue: "javascript",
+            content: [],  /**保存原始数据 */
+            activeRow: 0, /** 当前正在输入的行数*/
+            activeWord: 0, /** 当前正在输入的单词下标*/
+            inputVelocity: 0, /**速度*/
             inputAccuracy: 0, /** 准确率 */
         };
 
-        this.info= []; /**保存每行输入情况 */
+        this.info = []; /**保存每行输入情况 */
         this.startTime = 0;
-       
+
         this.formatData = this.formatData.bind(this);                           // 格式化原始数据      
         this.changeActiveRow = this.changeActiveRow.bind(this);                 // 行数发生改变   
         this.endInput = this.endInput.bind(this);                               // 输入完毕后    
         this.activeWordIndex = this.activeWordIndex.bind(this);                 // 单词下标发生变化
         this.computedInfo = this.computedInfo.bind(this);                       // 计算打字数组及正确率
+        this.changeSelect = this.changeSelect.bind(this);
     };
-    formatData(){
-        let result = [], row = [], len = 0;
-        jsData.forEach((word, i)=>{
+    formatData() {
+        let result = [], row = [], len = 0, data = [];
+        const { selectValue } = this.state;
+        switch (selectValue) {
+            case "javascript":
+                data = jsData;
+                break;
+            case "css":
+                data = cssData;
+                break;
+            case "英语打字":
+                data = testData;
+                break;
+            default:
+                data = testData;
+
+        }
+        data.forEach((word, i) => {
             row.push(word);
-            len += (word.text.length+1);
-            if(len >= 90){
+            len += (word.text.length + 1);
+            if (len >= 90) {
                 const item = row.pop();
                 result.push(row);
                 row = [item];
@@ -43,79 +67,94 @@ export default class SelfLayout extends Component {
         console.log(result)
         this.startTime = new Date().getTime();
     }
-    changeActiveRow(){
-        let {activeRow} = this.state;
-        activeRow ++;
+    changeActiveRow() {
+        let { activeRow } = this.state;
+        activeRow++;
         this.setState({
             activeRow: activeRow
         })
     }
-    activeWordIndex(index){
-        let {activeWord} = this.state;
-        if(index !== activeWord){  
+    activeWordIndex(index) {
+        let { activeWord } = this.state;
+        if (index !== activeWord) {
             this.setState({
                 activeWord: index
             })
         }
     }
-    endInput(){
+    endInput() {
+        let {inputVelocity, inputAccuracy} = this.state;
         console.log('end !!!');
+        alert( `End---速度：${inputVelocity}---正确率：${inputAccuracy}`)
     }
-    computedInfo(info){
-        const {activeRow} = this.state;
+    computedInfo(info) {
+        const { activeRow } = this.state;
         this.info[activeRow] = info;
         let len = 0;
         let errLen = 0;
-        this.info.forEach(row=>{
+        this.info.forEach(row => {
             len += row.length;
-            errLen += row.filter(i=>i).length
+            errLen += row.filter(i => i).length
         })
-        let a = ((errLen/len)*100).toFixed(2)
+        let a = ((errLen / len) * 100).toFixed(2)
 
         let nowDate = new Date().getTime();
-        let times = (nowDate - this.startTime)/1000/60;
-        let v = (len/times).toFixed(2)
+        let times = (nowDate - this.startTime) / 1000 / 60;
+        let v = (len / times).toFixed(2)
         this.setState({
-            inputAccuracy: isNaN(a)? 0.00: a,
+            inputAccuracy: isNaN(a) ? 0.00 : a,
             inputVelocity: v
         })
 
     }
-    render (){
-        const {content, activeRow, activeWord, inputAccuracy, inputVelocity} = this.state;
-        const word = content[activeRow] &&  content[activeRow][activeWord] && content[activeRow][activeWord];
-        const desc = word && word.desc || "" 
-        const url = word && word.url || "" 
+
+    changeSelect(e) {
+        const value = e.target.value;
+        this.setState({
+            selectValue: value
+        })
+    }
+
+    render() {
+        const { content, activeRow, activeWord, inputAccuracy, inputVelocity, selectValue } = this.state;
+        const word = content[activeRow] && content[activeRow][activeWord] && content[activeRow][activeWord];
+        const desc = (word && word.desc) || ""
+        const url = (word && word.url) || ""
         return (<div className={commonCss.content}>
-                <p>
-                    <span className={`${commonCss.button} primary`} onClick={this.formatData}>start</span>
-                </p>
-                <div style={{display: content.length>0 ?"block": "none", minHeight:"120px"}}>
-                    <p>正确率：{inputAccuracy}%</p>
-                    <p>速度：{inputVelocity} code/分</p>
-                    <br/>
-                    <p style={{color: "red"}}>{desc}<a href={url} target="_blank">api详情传送门</a></p>
-                    <br/>
-                    <hr/>
-                </div>
-                <div style={{height: "600px", overflow: "auto"}}>
-                    {
-                        content.map((row, index)=>{
-                            return (
-                                <Base content={row}
-                                key={index} 
-                                row={index} 
-                                isEnd={index === content.length-1}
+            <p>
+                <select value={selectValue} onChange={this.changeSelect} style={selectStyle}>
+                    <option value="javascript">javascript</option>
+                    <option value="css">css</option>
+                    <option value="英语打字">英语打字</option>
+                </select>
+                <span className={`${commonCss.button} primary`} onClick={this.formatData}>start</span>
+            </p>
+            <div style={{ display: content.length > 0 ? "block" : "none", minHeight: "120px" }}>
+                <p>正确率：{inputAccuracy}%</p>
+                <p>速度：{inputVelocity} code/分</p>
+                <br />
+                <p style={{ color: "red" }}>{desc}<a href={url} target="_blank">api详情传送门</a></p>
+                <br />
+                <hr />
+            </div>
+            <div style={{ height: "600px", overflow: "auto" }}>
+                {
+                    content.map((row, index) => {
+                        return (
+                            <Base content={row}
+                                key={index}
+                                row={index}
+                                isEnd={index === content.length - 1}
                                 activeRow={activeRow}
                                 changeActiveRow={this.changeActiveRow}
                                 endInput={this.endInput}
-                                activeWordIndex={this.activeWordIndex} 
+                                activeWordIndex={this.activeWordIndex}
                                 computedInfo={this.computedInfo} />
-                            )
-                        })
-                    }
-                </div>
-                    
-            </div>)
+                        )
+                    })
+                }
+            </div>
+
+        </div>)
     }
 }
